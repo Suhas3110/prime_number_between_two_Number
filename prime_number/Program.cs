42a17f8427de68eb36a927877020500ef90ae1e2
suhas <suhas@DELL>
2024-11-29 15:58:46 +0530

    Add project files.

---
+++ prime_number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace prime_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            Console.WriteLine("prime number between two number");
+            for(i=1;i<=10; i++)
+            {
+                for(j=2;j<=i;j++)
+                {
+                    if (i % j == 0)
+                        break;
+                }
+                if(i==j)
+                {
+                    Console.WriteLine(j);
+                }
+            }
+          Console.ReadKey();
+        }
+    }
+}
